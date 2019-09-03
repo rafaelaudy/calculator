7b2ecb637ae54ec9eb833b4dc745c342cb15b049
Rafael Audy Glanzner <rafael.audy@gmail.com>
2019-09-03 17:48:29 +0100

    Adds action creators for operations

--- src/ActionCreators.js
+++ src/ActionCreators.js
@@ -3,3 +3,23 @@ export const numberClick = number => ({
   type: NUMBER_CLICK_TYPE,
   payload: { value: number + "" }
 });
+
+export const SUM_CLICK_TYPE = "SUM_CLICK";
+export const sumClick = () => ({
+  type: SUM_CLICK_TYPE
+});
+
+export const SUBTRACTION_CLICK = "SUBTRACTION_CLICK";
+export const subtractionClick = () => ({
+  type: SUBTRACTION_CLICK
+});
+
+export const MULTIPLICATION_CLICK = "MULTIPLICATION_CLICK";
+export const multiplicationClick = () => ({
+  type: MULTIPLICATION_CLICK
+});
+
+export const DIVISION_CLICK = "DIVISION_CLICK";
+export const divisionClick = () => ({
+  type: DIVISION_CLICK
+});
--- src/ActionCreators.test.js
+++ src/ActionCreators.test.js
@@ -1,5 +1,27 @@
-import { numberClick } from "./ActionCreators";
+import {
+  numberClick,
+  sumClick,
+  subtractionClick,
+  multiplicationClick,
+  divisionClick
+} from "./ActionCreators";
 
 it("numberClick action creator exports the correct format", () => {
   expect(numberClick(1)).toMatchSnapshot();
+});
+
+it("sumClick action creator exports the correct format", () => {
+  expect(sumClick()).toMatchSnapshot();
+});
+
+it("subtractionClick action creator exports the correct format", () => {
+  expect(subtractionClick()).toMatchSnapshot();
+});
+
+it("multiplicationClick action creator exports the correct format", () => {
+  expect(multiplicationClick()).toMatchSnapshot();
+});
+
+it("divisionClick action creator exports the correct format", () => {
+  expect(divisionClick()).toMatchSnapshot();
 });
