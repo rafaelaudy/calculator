480a75a9506c4868aa723cab827ef5ee20e0e7d6
Rafael Audy Glanzner <rafael.audy@gmail.com>
2019-09-04 12:52:54 +0100

    Implements substration and add on app reducer

--- src/appReducer.js
+++ src/appReducer.js
@@ -1,9 +1,32 @@
-import { NUMBER_CLICK_TYPE, SUM_CLICK_TYPE } from "./ActionCreators";
+/*eslint-disable no-eval */
 
-const appReducer = (
-  state = { value: "0", temporaryValue: undefined },
-  { type, payload }
+import {
+  NUMBER_CLICK_TYPE,
+  SUM_CLICK_TYPE,
+  SUBTRACTION_CLICK,
+  CALCULATE_CLICK
+} from "./ActionCreators";
+
+const calculate = (
+  { temporaryOperation, temporaryValue, value },
+  currentOperation
 ) => {
+  if (temporaryOperation === "=") {
+    temporaryOperation = currentOperation;
+  }
+
+  return temporaryOperation
+    ? eval(temporaryValue + temporaryOperation + value) + ""
+    : value;
+};
+
+const defaultState = {
+  value: "0",
+  temporaryValue: undefined,
+  temporaryOperation: undefined
+};
+
+const appReducer = (state = defaultState, { type, payload }) => {
   switch (type) {
     case NUMBER_CLICK_TYPE: {
       const value =
@@ -11,12 +34,29 @@ const appReducer = (
       return { ...state, value };
     }
 
+    case CALCULATE_CLICK: {
+      return {
+        ...state,
+        temporaryValue: calculate(state),
+        temporaryOperation: "=",
+        value: "0"
+      };
+    }
+
     case SUM_CLICK_TYPE: {
       return {
         ...state,
-        temporaryValue: state.temporaryValue
-          ? Number(state.temporaryValue) + Number(state.value) + ""
-          : state.value,
+        temporaryValue: calculate(state, "+"),
+        temporaryOperation: "+",
+        value: "0"
+      };
+    }
+
+    case SUBTRACTION_CLICK: {
+      return {
+        ...state,
+        temporaryValue: calculate(state, "-"),
+        temporaryOperation: "-",
         value: "0"
       };
     }
--- src/appReducer.test.js
+++ src/appReducer.test.js
@@ -2,9 +2,48 @@ import {
   NUMBER_CLICK_TYPE,
   numberClick,
   SUM_CLICK_TYPE,
-  sumClick
+  sumClick,
+  SUBTRACTION_CLICK,
+  subtractionClick,
+  calculateClick
 } from "./ActionCreators";
 import appReducer from "./appReducer";
+
+describe("Calc operations used in conjuction", () => {
+  it("Always use the stored operation to do the calculation", () => {
+    let state = appReducer({ value: "0" }, numberClick("5"));
+    state = appReducer(state, sumClick());
+    state = appReducer(state, numberClick("5"));
+    state = appReducer(state, subtractionClick());
+    expect(state.temporaryValue).toBe("10");
+    state = appReducer(state, numberClick("5"));
+    state = appReducer(state, sumClick());
+    expect(state.temporaryValue).toBe("5");
+  });
+
+  it("Can continue operations after using the calculate function", () => {
+    let state = appReducer({ value: "0" }, numberClick("5"));
+    state = appReducer(state, sumClick());
+    state = appReducer(state, numberClick("5"));
+    state = appReducer(state, calculateClick());
+    expect(state.temporaryValue).toBe("10");
+    state = appReducer(state, numberClick("5"));
+    state = appReducer(state, sumClick());
+    state = appReducer(state, calculateClick());
+    expect(state.temporaryValue).toBe("15");
+  });
+});
+
+describe("Exception scenarios", () => {
+  it("should ignore inexting types", () => {
+    const newState = appReducer(undefined, { type: "INEXISTING" });
+    expect(newState).toEqual({
+      value: "0",
+      temporaryValue: undefined,
+      temporaryOperation: undefined
+    });
+  });
+});
 
 describe(`${NUMBER_CLICK_TYPE}`, () => {
   it("removes 0 on the start of the string when a number is added", () => {
@@ -12,7 +51,7 @@ describe(`${NUMBER_CLICK_TYPE}`, () => {
     expect(newState.value).toBe("1");
   });
 
-  it("keep only one 0 at the beggining of the string", () => {
+  it("keeps only one 0 at the beggining of the string", () => {
     const newState = appReducer({ value: "0" }, numberClick("0"));
     expect(newState.value).toBe("0");
   });
@@ -23,28 +62,44 @@ describe(`${NUMBER_CLICK_TYPE}`, () => {
   });
 });
 
-describe(`${SUM_CLICK_TYPE}`, () => {
-  it("clears current value", () => {
-    const newState = appReducer(
-      { value: "5", temporaryValue: "1" },
-      sumClick()
-    );
-    expect(newState.value).toBe("0");
-  });
+[
+  {
+    type: SUM_CLICK_TYPE,
+    method: sumClick,
+    operation: "+",
+    result: "5"
+  },
+  {
+    type: SUBTRACTION_CLICK,
+    method: subtractionClick,
+    operation: "-",
+    result: "-1"
+  }
+].map(scenario => {
+  describe(scenario.type, () => {
+    it("clears current value and store as temporary", () => {
+      const newState = appReducer({ value: "5" }, scenario.method());
+      expect(newState.value).toBe("0");
+      expect(newState.temporaryValue).toBe("5");
+    });
 
-  it("sum current value with the temporary one", () => {
-    const newState = appReducer(
-      { value: "5", temporaryValue: "1" },
-      sumClick()
-    );
-    expect(newState.temporaryValue).toBe("6");
-  });
+    it("stores operation as +", () => {
+      const newState = appReducer({}, scenario.method());
+      expect(newState.temporaryOperation).toBe(scenario.operation);
+    });
 
-  it("if no temporary value is stored, value is stored as temporary", () => {
-    const newState = appReducer(
-      { value: "5", temporaryValue: undefined },
-      sumClick()
-    );
-    expect(newState.temporaryValue).toBe("5");
+    it(`calculates ${scenario.operation} in case a temporary value and operation are present`, () => {
+      const newState = appReducer(
+        {
+          value: "3",
+          temporaryValue: "2",
+          temporaryOperation: scenario.operation
+        },
+        scenario.method()
+      );
+      expect(newState.temporaryValue).toBe(scenario.result);
+      expect(newState.temporaryOperation).toBe(scenario.operation);
+      expect(newState.value).toBe("0");
+    });
   });
 });
